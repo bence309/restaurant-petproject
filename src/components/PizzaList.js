33e92a30888a619c75e526ab2fa9e94e2e359ba0
Bence Bodi <bence309@hotmail.com>
2023-11-16 13:41:00 +0100

    successfully fetched pizzas on /pizza

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getPizzas } from '../api';
 
 const PizzaList = () => {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    // Fetch pizza data when the component mounts
+    const fetchPizzas = async () => {
+      try {
+        const pizzaData = await getPizzas();
+        setPizzas(pizzaData);
+      } catch (error) {
+        console.error('Error fetching pizzas:', error);
+      }
+    };
+
+    fetchPizzas();
+  }, []); 
+
   return (
     <div>
       <h2>Pizza List</h2>
-      {/* Add your pizza content here */}
+      <ul>
+        {pizzas.map((pizza) => (
+          <li key={pizza.id}>
+            <h3>{pizza.name}</h3>
+            <p>Ingredients: {pizza.ingredients.join(', ')}</p>
+            <p>Price: ${pizza.price}</p>
+            <img src={pizza.image} alt={pizza.name} style={{ maxWidth: '200px' }} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
