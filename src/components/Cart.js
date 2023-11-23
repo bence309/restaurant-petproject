ddefd87f1cf4be8c6aef55de843bf1623fd9ecb7
Bence Bodi <bence309@hotmail.com>
2023-11-23 10:44:07 +0100

    added total price calculations to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
 const Cart = ({ cart, removeFromCart }) => {
+
+  const total = cart.reduce((acc, item) => acc + item.price, 0);
+
   return (
-    <div className="item-list">
-      {cart.map((item) => (
-        <div key={item.id} className="item-card">
-          <h3>{item.name}</h3>
-          <p>Price: ${item.price}</p>
-          <img src={item.image} alt={item.name} style={{ maxWidth: '200px' }} />
-          <br/>
-          <button onClick={() => removeFromCart(item.id)}>Delete</button>
-        </div>
-      ))}
+    <div>
+      <h2>Shopping Cart</h2>
+      <div className="item-list">
+        {cart.map((item) => (
+          <div key={item.id} className="item-card">
+            <h3>{item.name}</h3>
+            <p>Price: ${item.price}</p>
+            <img src={item.image} alt={item.name} style={{ maxWidth: '200px' }} />
+            <button onClick={() => removeFromCart(item.id)}>Delete</button>
+          </div>
+        ))}
+      </div>
+      {/* Display the total price */}
+      <p>Total Price: ${total}</p>
     </div>
   );
 };
