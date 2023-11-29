c5abccfe7d36cb37dbeb7a171b65c539f815bb6f
Bence Bodi <bence309@hotmail.com>
2023-11-29 12:41:18 +0100

    updated registration form

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -3,22 +3,23 @@ import axios from 'axios';
 
 const RegistrationForm = () => {
   const [formData, setFormData] = useState({
-    
     username: '',
     email: '',
     password: '',
-    
   });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      
+      // Send registration data to the server
       const response = await axios.post('http://localhost:3002/api/register', formData);
-      console.log(response.data); 
+      console.log(response.data); // Handle success or display a success message
+
+      // Assuming you have a /register route for successful registration
+      window.location.href = '/register';
     } catch (error) {
-      console.error('Error registering user:', error); 
+      console.error('Error registering user:', error); // Handle error or display an error message
     }
   };
 
@@ -30,14 +31,25 @@ const RegistrationForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      {/* Render your form fields here, and connect them to the state with onChange */}
-      <input type="text" name="username" value={formData.username} onChange={handleChange} />
-      <input type="email" name="email" value={formData.email} onChange={handleChange} />
-      <input type="password" name="password" value={formData.password} onChange={handleChange} />
-      {/* Add more fields as needed */}
-      <button type="submit">Register</button>
-    </form>
+    <div>
+      <h2>Registration</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" value={formData.username} onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+      
+    </div>
   );
 };
 
