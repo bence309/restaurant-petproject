3c1f62126469721fb45366dc368643393af9e156
Bence Bodi <bence309@hotmail.com>
2023-12-05 15:43:54 +0100

    fixed dark mode background and mongodb connection

--- src/components/App.js
+++ src/components/App.js
@@ -12,12 +12,12 @@ import Cart from './Cart';
 import RegistrationForm from './RegistrationForm';
 import ScrollToTop from './ScrollToTop';
 import './ScrollToTop.css';
-import './App.css'
+import './App.css';
 
 const App = () => {
   const [cart, setCart] = useState(JSON.parse(localStorage.getItem('cart')) || []);
   const [message, setMessage] = useState('');
-  const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
     localStorage.setItem('cart', JSON.stringify(cart));
@@ -39,7 +39,7 @@ const App = () => {
     console.log('Updated cart:', updatedCart);
     setCart(updatedCart);
     setMessage('Item removed from cart!');
-  
+
     setTimeout(() => {
       setMessage('');
     }, 1000);
@@ -47,50 +47,52 @@ const App = () => {
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
-
   };
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-mode', darkMode);
+  }, [darkMode]);
 
   return (
     <Router>
-      <div className={darkMode ? 'dark-mode' : 'light-mode'}>
-      <Navigation /> {/* Move inside the Router */}
-      <div className="dark-mode-toggle">
+      <div className={darkMode ? 'app-container dark-mode' : 'app-container light-mode'}>
+        <Navigation />
+        <div className="dark-mode-toggle">
           <button onClick={toggleDarkMode} className="dark-mode-button">
             {darkMode ? 'Toggle Light Mode' : 'Toggle Dark Mode'}
           </button>
         </div>
-      <Routes>
-        <Route path="/pizza" element={<PizzaList addToCart={addToCart} />} /> {/* Pass addToCart prop */}
-        <Route path="/drinks" element={<DrinkList addToCart={addToCart} />} />
-        <Route path="/dessert" element={<DessertList addToCart={addToCart} />} />
-        <Route path="/sauces" element={<SauceList addToCart={addToCart} />} />
-        <Route path="/pasta" element={<PastaList addToCart={addToCart} />} />
-        <Route path="/sides" element={<SideList addToCart={addToCart} />} />
-        <Route path="/salads" element={<SaladList addToCart={addToCart} />} />
-        <Route path="/cart" element={<Cart cart={cart} removeFromCart={removeFromCart} />} />
-        <Route path="/register" element={<RegistrationForm />} />
-      </Routes>
-      <div>
-        {/* Display the message with styling */}
-        {message && (
-          <div
-            style={{
-              position: 'fixed',
-              top: '70%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              background: 'lightgreen',
-              padding: '20px',
-              borderRadius: '5px',
-              boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
-              zIndex: 999,
-            }}
-          >
-            <p>{message}</p>
-          </div>
-        )}
-        <ScrollToTop />
-      </div>
+        <Routes>
+          <Route path="/pizza" element={<PizzaList addToCart={addToCart} />} />
+          <Route path="/drinks" element={<DrinkList addToCart={addToCart} />} />
+          <Route path="/dessert" element={<DessertList addToCart={addToCart} />} />
+          <Route path="/sauces" element={<SauceList addToCart={addToCart} />} />
+          <Route path="/pasta" element={<PastaList addToCart={addToCart} />} />
+          <Route path="/sides" element={<SideList addToCart={addToCart} />} />
+          <Route path="/salads" element={<SaladList addToCart={addToCart} />} />
+          <Route path="/cart" element={<Cart cart={cart} removeFromCart={removeFromCart} />} />
+          <Route path="/register" element={<RegistrationForm />} />
+        </Routes>
+        <div>
+          {message && (
+            <div
+              style={{
+                position: 'fixed',
+                top: '70%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                background: 'lightgreen',
+                padding: '20px',
+                borderRadius: '5px',
+                boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+                zIndex: 999,
+              }}
+            >
+              <p>{message}</p>
+            </div>
+          )}
+          <ScrollToTop />
+        </div>
       </div>
     </Router>
   );
